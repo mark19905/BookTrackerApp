6f72342f91b71107aba18e3eb414d2575c7c4cf4
Mark Lopez <mark19905@gmail.com>
2023-10-13 19:28:38 -0500

    MAL - Implemented final changes to Web App Project.

--- BookTracker/src/main/java/com/booktracker/SimpleFormSearch.java
+++ BookTracker/src/main/java/com/booktracker/SimpleFormSearch.java
@@ -26,69 +26,90 @@ public class SimpleFormSearch extends HttpServlet {
 	      search(keyword, response);
 	   }
 
-	   void search(String keyword, HttpServletResponse response) throws IOException {
-	      response.setContentType("text/html");
-	      PrintWriter out = response.getWriter();
-	      String title = "Database Result";
-	      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n"; 
-	      out.println(docType + "<html>\n" + "<head><title>" + title + "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n" + "<h1 align=\"center\">" + title + "</h1>\n");
+   void search(String keyword, HttpServletResponse response) throws IOException {
+	    response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+	    String title = "Your Book Library";
+	    String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+	    
+	    // Add CSS
+	    String styles = "<style>" + 
+                "table { width: 100%; border-collapse: collapse; }" +
+                "thead { background-color: green; color: white; }" +  // This line has been modified
+                "tr { color: black; }" +  // This line has been added
+                "td:nth-child(odd), th:nth-child(odd) { background-color: white; }" +
+                "td:nth-child(even), th:nth-child(even) { background-color: lightgrey; }" +
+                "</style>";
 
-	      Connection connection = null;
-	      PreparedStatement preparedStatement = null;
-	      try {
-	         DBConnection.getDBConnection();
-	         connection = DBConnection.connection;
+	    out.println(docType + 
+	                "<html>\n" + 
+	                "<head><title>" + title + "</title>" + styles + "</head>\n" + 
+	                "<body bgcolor=\"#f0f0f0\">\n" + 
+	                "<h1 align=\"center\">" + title + "</h1>\n");
+	    
+	    Connection connection = null;
+	    PreparedStatement preparedStatement = null;
+	    ResultSet rs = null;  // Declare rs here
 
-	         if (keyword.isEmpty()) {
+	    try {
+	        DBConnection.getDBConnection();
+	        connection = DBConnection.connection;
+
+	        if (keyword.isEmpty()) {
 	            String selectSQL = "SELECT * FROM books";
 	            preparedStatement = connection.prepareStatement(selectSQL);
-	         } else {
+	        } else {
 	            String selectSQL = "SELECT * FROM books WHERE title LIKE ?";
 	            preparedStatement = connection.prepareStatement(selectSQL);
 	            preparedStatement.setString(1, "%" + keyword + "%");  // Searching in title column
-	         }
-	         ResultSet rs = preparedStatement.executeQuery();
+	        }
+	        rs = preparedStatement.executeQuery();  // Initialize rs here
 
-	         while (rs.next()) {
-	        	    int id = rs.getInt("id");
-	        	    String bookTitle = rs.getString("title").trim(); // Rename to bookTitle
-	        	    String isbn = rs.getString("isbn").trim();
-	        	    String author_name = rs.getString("author_name").trim();
-	        	    String genre = rs.getString("genre").trim();
-	        	    String reading_status = rs.getString("reading_status").trim();
-	        	    int rating = rs.getInt("rating");
+	        out.println("<table border='1' align='center'>");
+	        out.println("<tr><th>ID</th><th>Title</th><th>ISBN</th><th>Author</th><th>Genre</th><th>Reading Status</th><th>Rating</th></tr>");
+	        while (rs.next()) {
+	            int id = rs.getInt("id");
+	            String bookTitle = rs.getString("title").trim();
+	            String isbn = rs.getString("isbn").trim();
+	            String author_name = rs.getString("author_name").trim();
+	            String genre = rs.getString("genre").trim();
+	            String reading_status = rs.getString("reading_status").trim();
+	            int rating = rs.getInt("rating");
 
-	        	    out.println("ID: " + id + ", ");
-	        	    out.println("Title: " + bookTitle + ", "); // Use bookTitle here
-	        	    out.println("ISBN: " + isbn + ", ");
-	        	    out.println("Author: " + author_name + ", ");
-	        	    out.println("Genre: " + genre + ", ");
-	        	    out.println("Reading Status: " + reading_status + ", ");
-	        	    out.println("Rating: " + rating + "<br>");
-	        	}
-	         out.println("<a href=/BookTracker/SimpleFormSearch.html>Search Data</a> <br>");
-	         out.println("</body></html>");
-	         rs.close();
-	         preparedStatement.close();
-	         connection.close();
-	      } catch (SQLException se) {
-	         se.printStackTrace();
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      } finally {
-	         try {
-	            if (preparedStatement != null)
-	               preparedStatement.close();
-	         } catch (SQLException se2) {
-	         }
-	         try {
-	            if (connection != null)
-	               connection.close();
-	         } catch (SQLException se) {
+	            out.println("<tr>");
+	            out.println("<td>" + id + "</td>");
+	            out.println("<td>" + bookTitle + "</td>");
+	            out.println("<td>" + isbn + "</td>");
+	            out.println("<td>" + author_name + "</td>");
+	            out.println("<td>" + genre + "</td>");
+	            out.println("<td>" + reading_status + "</td>");
+	            out.println("<td>" + rating + "</td>");
+	            out.println("</tr>");
+	        }
+	        out.println("</table>");
+	        out.println("<a href=/BookTracker/SimpleFormSearch.html>Search Data</a> <br>");
+	        out.println("</body></html>");
+
+	    } catch (SQLException se) {
+	        se.printStackTrace();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (rs != null) {
+	                rs.close();
+	            }
+	            if (preparedStatement != null) {
+	                preparedStatement.close();
+	            }
+	            if (connection != null) {
+	                connection.close();
+	            }
+	        } catch (SQLException se) {
 	            se.printStackTrace();
-	         }
-	      }
-	   }
+	        }
+	    }
+	}
 
 	   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	      doGet(request, response);
